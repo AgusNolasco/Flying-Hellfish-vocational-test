043763a3dc9087cfb86a68c151929057d284c082
luchox73 <luchox73x@gmail.com>
2021-05-11 22:12:23 -0300

    outcome.rb added. Respective migration to create the table added. outcome.rb added to init file

---
+++ db/migrations/00008_create_outcomes_table.rb
@@ -0,0 +1,16 @@
+Sequel.migration do
+    up do
+      create_table(:outcomes) do
+        primary_key   :id
+        foreign_key   :career_id, :careers
+        foreign_key   :choice_id, :choices
+        DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+        DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+      end
+    end
+  
+    down do
+      drop_table(:outcomes)
+    end
+  end
+
