2c8c4af546a84be434d28590d26f29c92ecf1f29
Agustin Nolasco <franconolasco99@gmail.com>
2021-05-16 19:59:42 -0300

    More migrations fixed

--- db/migrations/00009_add_choice_to_question_association.rb
+++ db/migrations/00009_add_choice_to_question_association.rb
@@ -1,7 +1,6 @@
 Sequel.migration do
   up do
     alter_table :choices do
-      add_foreign_key :outcome_id, :outcomes
       add_foreign_key :question_id, :questions
     end
   end
@@ -10,7 +9,6 @@ Sequel.migration do
 
   down do
     alter_table :choices do
-      drop_foreign_key :outcome_id
       drop_foreign_key :question_id
     end
   end
