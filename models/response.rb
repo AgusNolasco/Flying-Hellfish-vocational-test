64ca210e6b59f9d31b5677923864bd4f761e9308
Agustin Nolasco <franconolasco99@gmail.com>
2021-05-20 22:53:11 -0300

    add a test for response, and validate setting in the model

--- models/response.rb
+++ models/response.rb
@@ -1,5 +1,11 @@
 class Response < Sequel::Model
-    many_to_one :survey
-    many_to_one :question
-    one_to_one  :choice
+  many_to_one :survey
+  many_to_one :question
+  one_to_one  :choice
+  def validate
+    super
+    errors.add(:question_id, :name => 'question_id can not be nil') if not (question_id) or (question_id == nil)
+    errors.add(:choice_id,   :name => 'choice_id can not be nil')   if not (choice_id)   or (choice_id == nil)
+    errors.add(:survey_id,   :name => 'survey_id can not be nil')   if not (survey_id)   or (survey_id == nil)
+  end
 end
