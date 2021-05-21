b3d460131f9f1ee3b2a3283f42bebf398b2fc790
luchox73 <luchox73x@gmail.com>
2021-05-20 21:01:11 -0300

    add a test for outcome

--- models/outcome.rb
+++ models/outcome.rb
@@ -1,4 +1,10 @@
 class Outcome < Sequel::Model
 	many_to_one :career
 	many_to_one :choice
+	
+	def validate
+		super
+		errors.add(:career_id, :name => 'career_id can not be nil') if not (career_id) or (career_id == nil)
+		errors.add(:choice_id, :name => 'choice_id can not be nil') if not (choice_id) or (choice_id == nil)
+	end
 end
