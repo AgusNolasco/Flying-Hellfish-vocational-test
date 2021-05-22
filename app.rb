61775caccfff026b82a1576ed173b21dcf5e1b5a
Agustin Nolasco <franconolasco99@gmail.com>
2021-05-22 18:13:31 -0300

    Add a view for careers, links for each career, and a form for post careers

--- app.rb
+++ app.rb
@@ -1,43 +1,49 @@
 require './models/init.rb'
 
 class App < Sinatra::Base
-    get '/' do
-        "Hello World"
-    end
+  get '/' do
+    "Hello World"
+  end
 
-    get "/hello/:name" do
-        @name = params[:name]
-        erb :hello_template
-    end
+  get "/hello/:name" do
+    @name = params[:name]
+    erb :hello_template
+  end
 
-    post "/careers" do
-        data = JSON.parse request.body.read
-        career = Career.new(name: data['name'])
-        if career.save
-            [201, { 'Location' => "careers/#{career.id}" }, 'CREATED']
-        else
-            [500, {}, 'Internal Server Error']
-        end
+  post "/careers" do
+    data = request.body.read
+    career = Career.new(name: params[:name])
+    if career.save
+      [201, { 'Location' => "careers/#{career.id}" }, 'CREATED']
+    else
+      [500, {}, 'Internal Server Error']
     end
+  end
 
-    get '/careers' do
-        Career.all.map { |c| c.name + "\n" }
-    end
+  get '/careers' do
+    @careers = Career.all
 
-    post "/posts" do
-        request.body.rewind  # in case someone already read it
-        data = JSON.parse request.body.read
-        post = Post.new(description: data['desc'])
-        if post.save
-            [201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
-        else
-            [500, {}, 'Internal Server Error']
-        end
-    end
+    erb :careers_index
+  end
 
-    get '/posts' do
-        p = Post.where(id: 1).last
-        p.description
+  get '/careers/:id' do
+    career = Career.find(id: params[:id])
+    "Career's name: #{career.name}"
+  end
+
+  post "/posts" do
+    request.body.rewind  # in case someone already read it
+    data = JSON.parse request.body.read
+    post = Post.new(description: data['desc'])
+    if post.save
+      [201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
+    else
+      [500, {}, 'Internal Server Error']
     end
+  end
+
+  get '/posts' do
+    p = Post.where(id: 1).last
+    p.description
+  end
 end
-
