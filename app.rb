fd1aba606a8ab9c563da128f4bf301a669f31d08
Agustin Nolasco <franconolasco99@gmail.com>
2021-06-08 20:35:34 -0300

    Added some comment to the code, and remove the method GET from surveys and GET from /hello/:name

--- app.rb
+++ app.rb
@@ -1,13 +1,9 @@
 require './models/init.rb'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :landing_page
-  end
-
-  get '/hello/:name' do
-    @name = params[:name]
-    erb :hello_template
   end
 
   post '/careers' do
@@ -20,12 +16,13 @@ class App < Sinatra::Base
     end
   end
 
+  #Show all the careers
   get '/careers' do
     @careers = Career.all
-
     erb :careers_index
   end
 
+  #Show information about a determined career (by id)
   get '/careers/:id' do
     @career = Career.find(id: params[:id])
     erb :careers_template
@@ -34,29 +31,26 @@ class App < Sinatra::Base
   post '/surveys' do
     data = request.body.read
     survey = Survey.new(username: params[:username])
-    if Question.first
+    if Question.first #if we have at least one question
       if survey.save
         [201, { 'Location' => "surveys/#{survey.id}" }, 'CREATED']
       else
         [500, {}, 'Internal Server Error']
       end
       first_question_id = Question.first.id 
+      #Redirect us to the first question
       redirect to("/questions/#{first_question_id}?survey_id=#{survey.id}")
     else
+      #if we have no questions then finish
       redirect '/finish'
     end
   end
 
-  get '/surveys' do
-    survey = Survey.all.map { |s| s.username }
-    survey
-  end
-
   get '/questions/:id' do
-  	if params[:id].to_i > Question.last.id 
+  	if params[:id].to_i > Question.last.id #Check if the last question was asked 
       redirect "/finish/#{params[:survey_id]}"
     end
-    if Question.find(id: params[:id]).nil?
+    if Question.find(id: params[:id]).nil? #Check if the currect question(id) is nil
     	redirect to("/questions/#{(params[:id].to_i) + 1}?survey_id=#{params[:survey_id]}")
     end
     @question = Question.find(id: params[:id])
@@ -68,6 +62,7 @@ class App < Sinatra::Base
     response = Response.create(question_id: params[:question_id], choice_id: params[:choice_id], survey_id: params[:survey_id])
     if response.save
       [201, { 'Location' => "responses/#{response.id}" }, 'CREATED']
+      #Redirect us to the next question
       redirect to("/questions/#{((response.question_id) + 1)}?survey_id=#{params[:survey_id]}")
     else
       [500, {}, 'Internal Server Error']
@@ -90,12 +85,14 @@ class App < Sinatra::Base
     p.description
   end
 
+  #This is executed when there are no questions
   get '/finish' do
     erb :no_question_template
   end
 
   get '/finish/:survey_id' do
     @survey = Survey.find(:id => params[:survey_id])
+    #Use a HashMap to index the careers
     careersCount = Hash.new
     for c in Career.all
       careersCount[c.id] = 0
