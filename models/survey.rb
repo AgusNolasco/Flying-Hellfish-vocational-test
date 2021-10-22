6ed80296e2aeba2398ab0be5da28723b4c3af848
luchox73 <luchox73x@gmail.com>
2021-10-22 16:32:51 -0300

    [#179997458] more refactor to compute_result method from survey model

--- models/survey.rb
+++ models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < Sequel::Model
 	many_to_one :career
 	one_to_many :responses
-	
+
 	def validate
 		super
 		errors.add(:username, :name => 'username can not be nil or empty') if username.nil? || username.empty?
@@ -9,16 +9,13 @@ class Survey < Sequel::Model
 
   def compute_result
     careers_count = Hash.new
-    for career in Career.all
-      careers_count[career.id] = 0
+    Career.all.each { |career| careers_count[career.id] = 0 }
+
+    self.responses.each do |response|
+      Choice.find(id: response.choice_id)
+      	.outcomes.each { |outcome| careers_count[outcome.career_id] += 1 }
     end
-    
-    for response in self.responses
-      for outcome in Choice.find(id: response.choice_id).outcomes
-        careers_count[outcome.career_id] += 1
-      end
-    end
-    
+
     self.update(
     	career_id: careers_count.key(careers_count.values.max),
     	completed_at: Sequel.lit('NOW()')
